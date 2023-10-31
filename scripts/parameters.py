723ddea250f0b83f053ecf070338e17ebdfef2b5
fzocola <flo.zocola@gmail.com>
2023-10-31 16:37:04 +0100

    main file

---
+++ scripts/parameters.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+# Project directories paths
+paths = {'main': Path.cwd()}
+
+paths.update({'data': Path.joinpath(paths.get('main'), 'data'),
+              'output': Path.joinpath(paths.get('main'), 'output'),
+              'scripts': Path.joinpath(paths.get('main'), 'scripts')})
+
