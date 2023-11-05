ba08aec769acffd15edfeb774403e2b11bb4630a
fzocola <flo.zocola@gmail.com>
2023-11-05 18:55:56 +0100

    main file

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ import numpy as np
 
 
 
-#file_data_1 = Path.joinpath(paths.get('Data'), 'qcyz6yksc03op9l6w.sas7bdat')
+
+
+#file_data_1 = Path.joinpath(paths.get('Data'), 'CRSP_Compustat_Merged_Fundamentals_Quarterly.sas7bdat')
 #fundamentals_quarterly_data = pd.read_sas(file_data_1)
 
-file_data_2 = Path.joinpath(paths.get('data'), 'qpnseppzotdph6x8j.sas7bdat')
+file_data_2 = Path.joinpath(paths.get('data'), 'CRSP_Compustat_Merged_Security_Monthly.sas7bdat')
 security_monthly_data = pd.read_sas(file_data_2)
 
+#file_data_3 = Path.joinpath(paths.get('data'), 'CRSP_Security_Files_Monthly_Stock.sas7bdat')
+#security_monthly_data = pd.read_sas(file_data_3)
 
+'''
 security_monthly_data_filter = security_monthly_data.sort_values(by=['GVKEY', 'DATADATE'])
 security_monthly_data_filter = security_monthly_data_filter.loc[(security_monthly_data_filter['DATADATE'] >= '1990-01-01')]
 security_monthly_data_filter = security_monthly_data_filter.loc[(security_monthly_data_filter['CSHTRM']*security_monthly_data_filter['PRCCM'] >= 100000000)]
@@ -29,3 +34,4 @@ n = security_monthly_data_filter_2['GVKEY'].nunique()
 n = security_monthly_data_filter['GVKEY'].nunique()
 name = pd.DataFrame(np.unique(security_monthly_data_filter[['CONML']]))
 
+'''
