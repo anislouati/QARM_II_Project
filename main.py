6544f50e77c1484fd4d2af287876574f6d22e69c
fzocola <flo.zocola@gmail.com>
2023-11-05 23:49:09 +0100

    Test

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from scripts.classes import paths
 
 
 
-#file_data_1 = Path.joinpath(paths.get('Data'), 'CRSP_Compustat_Merged_Fundamentals_Quarterly.sas7bdat')
+#file_data_1 = Path.joinpath(paths.get('Data'), 'crsp_compustat_merged_fundamentals_quarterly.sas7bdat')
 #fundamentals_quarterly_data = pd.read_sas(file_data_1)
 
-file_data_2 = Path.joinpath(paths.get('data'), 'CRSP_Compustat_Merged_Security_Monthly.sas7bdat')
+file_data_2 = Path.joinpath(paths.get('data'), 'crsp_compustat_merged_security_monthly.sas7bdat')
 security_monthly_data = pd.read_sas(file_data_2)
 
-#file_data_3 = Path.joinpath(paths.get('data'), 'CRSP_Security_Files_Monthly_Stock.sas7bdat')
+#file_data_3 = Path.joinpath(paths.get('data'), 'crsp_security_files_monthly_stock.sas7bdat')
 #security_monthly_data = pd.read_sas(file_data_3)
