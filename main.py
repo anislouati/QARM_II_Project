7e2b4d1fea490488fa75a2affbad36fac9171993
alouati77 <louati.anis@outlook.com>
2023-11-28 14:59:51 +0100

    Data management

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # Import packages
 from pathlib import Path
+from scripts.functions import paths
 import numpy as np
 import pandas as pd
-from scripts.functions import paths
+import scripts.functions as fn
+
+# Project settings
+pd.set_option('display.width', 400)
+pd.set_option('display.max_columns', 10)
 
 
 # %%
@@ -28,6 +33,13 @@ df_security_monthly = df_security_monthly[ls_selected_cols_2]
 df_fundamentals_quarterly = df_fundamentals_quarterly.sort_values(by=['GVKEY', 'DATADATE'], ascending=[True, True])
 df_security_monthly = df_security_monthly.sort_values(by=['GVKEY', 'DATADATE'], ascending=[True, True])
 
+# Summarize data
+df_1 = df_fundamentals_quarterly.drop(columns=['GVKEY', 'DATADATE', 'CONM', 'TIC', 'CIK', 'GSECTOR', 'LOC'])
+df_summary_1 = fn.tab_summary(df_1)
+
+df_2 = df_security_monthly.drop(columns=['GVKEY', 'DATADATE'])
+df_summary_2 = fn.tab_summary(df_2)
+
 # Create year and quarter cols
 df_security_monthly['FYEARQ'] = df_security_monthly['DATADATE'].dt.year.astype(float)
 df_security_monthly['FQTR'] = df_security_monthly['DATADATE'].dt.quarter.astype(float)
@@ -35,9 +47,17 @@ df_security_monthly['FQTR'] = df_security_monthly['DATADATE'].dt.quarter.astype(
 ls_selected_cols_2 = ['GVKEY', 'DATADATE', 'FYEARQ', 'FQTR', 'CSHOQ', 'CSHTRM', 'PRCCM', 'TRT1M']
 df_security_monthly = df_security_monthly[ls_selected_cols_2]
 
+# Filter dates (min_year-)
+min_year = 1990
+df_fundamentals_quarterly = df_fundamentals_quarterly[df_fundamentals_quarterly['FYEARQ'] >= min_year]
+df_security_monthly = df_security_monthly[df_security_monthly['FYEARQ'] >= min_year]
+
+#
+#
+df_fundamentals_quarterly.isna().sum() / len(df_fundamentals_quarterly)
+df_security_monthly.isna().sum() / len(df_security_monthly)
 # Create year and quarter column
 # Filter dates (
-
 print(df_fundamentals_quarterly[['DATADATE', 'NIY']].isna().sum())
 print(df_fundamentals_quarterly[['DATADATE', 'NIQ']].isna().sum())
 print(df_fundamentals_quarterly['DATADATE'])
--- scripts/functions.py
+++ scripts/functions.py
@@ -1,5 +1,12 @@
 # Import packages
 from pathlib import Path
+import pandas as pd
+
+# Project directories paths (README: modify if necessary!)
+paths = {'main': Path.cwd()}
+paths.update({'data': Path.joinpath(paths.get('main'), 'data'),
+              'output': Path.joinpath(paths.get('main'), 'output'),
+              'scripts': Path.joinpath(paths.get('main'), 'scripts')})
 
 
 # %%
@@ -7,8 +14,14 @@ from pathlib import Path
 # *** Branch: DATA MANAGEMENT                    ***
 # **************************************************
 
-# Project directories paths (README: modify if necessary!)
-paths = {'main': Path.cwd()}
-paths.update({'data': Path.joinpath(paths.get('main'), 'data'),
-              'output': Path.joinpath(paths.get('main'), 'output'),
-              'scripts': Path.joinpath(paths.get('main'), 'scripts')})
+def tab_summary(df_data):
+    df_summary = pd.DataFrame({'Count': df_data.count(),  # Count of non-missing values
+                               'Missing Pct': (df_data.isna().sum() / len(df_data)),  # Missing values as percentage
+                               'Mean': df_data.mean(),
+                               'Min': df_data.min(),
+                               '25%': df_data.quantile([0.25]),
+                               '50%': df_data.quantile([0.50]),
+                               '75%': df_data.quantile([0.75]),
+                               'Max': df_data.max()})
+    return df_summary
+
