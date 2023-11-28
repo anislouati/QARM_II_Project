93ace36dc650055f967dadb675193d80903d9636
alouati77 <louati.anis@outlook.com>
2023-11-28 17:02:44 +0100

    Data management

--- main.py
+++ main.py
@@ -20,6 +20,13 @@ df_fundamentals_quarterly = pd.read_sas(Path.joinpath(paths.get('data'), 'crsp_c
 df_security_monthly = pd.read_sas(Path.joinpath(paths.get('data'), 'crsp_compustat_merged_security_monthly.sas7bdat'))
 
 # Filter selected cols
+
+# Filter sele
+# Filter selected col
+ls_selected_cols_1 = ['GVKEY', 'DATADATE', 'FYEARQ', 'FQTR', 'CONM', 'TIC', 'CIK', 'EXCHG', 'GSECTOR', 'LOC',
+                      'ACTQ', 'ATQ', 'CEQQ', 'CHEQ', 'COGSQ', 'CSTKQ', 'DLCQ', 'DLTTQ', 'DPQ', 'EPSFXQ',
+                      'LCTQ', 'LTQ', 'MIBTQ', 'NIMQ', 'NIQ', 'PIQ', 'PSTKQ', 'REQ', 'REVTQ', 'TEQQ',
+                      'TXPQ', 'WCAPQ', 'CAPXY', 'CSHTRQ', 'MKVALTQ', 'PRCCQ']
 ls_selected_cols_1 = ['GVKEY', 'DATADATE', 'FYEARQ', 'FQTR', 'CONM', 'TIC', 'CIK', 'EXCHG', 'GSECTOR', 'LOC',
                       'ACTQ', 'ATQ', 'CEQQ', 'CHEQ', 'COGSQ', 'CSTKQ', 'DLCQ', 'DLTTQ', 'DPQ', 'EPSFXQ',
                       'LCTQ', 'LTQ', 'MIBTQ', 'NIMQ', 'NIQ', 'PIQ', 'PSTKQ', 'REQ', 'REVTQ', 'TEQQ',
@@ -33,6 +40,28 @@ df_security_monthly = df_security_monthly[ls_selected_cols_2]
 df_fundamentals_quarterly = df_fundamentals_quarterly.sort_values(by=['GVKEY', 'DATADATE'], ascending=[True, True])
 df_security_monthly = df_security_monthly.sort_values(by=['GVKEY', 'DATADATE'], ascending=[True, True])
 
+# Create year and quarter cols
+df_fundamentals_quarterly['FYEAR'] = df_fundamentals_quarterly['DATADATE'].dt.year.astype(float)
+df_fundamentals_quarterly['FQTR'] = df_fundamentals_quarterly['DATADATE'].dt.quarter.astype(float)
+
+df_security_monthly['FYEARQ'] = df_security_monthly['DATADATE'].dt.year.astype(float)
+df_security_monthly['FQTR'] = df_security_monthly['DATADATE'].dt.quarter.astype(float)
+
+ls_selected_cols_2 = ['GVKEY', 'DATADATE', 'FYEARQ', 'FQTR', 'CSHOQ', 'CSHTRM', 'PRCCM', 'TRT1M']
+df_security_monthly = df_security_monthly[ls_selected_cols_2]
+
+
+
+
+
+
+# Filter dates (min_year-)
+min_year = 1990
+max_year = 2023
+df_fundamentals_quarterly = df_fundamentals_quarterly[(df_fundamentals_quarterly['FYEARQ'] >= min_year) & (df_fundamentals_quarterly['FYEARQ'] <= max_year)]
+df_security_monthly = df_security_monthly[(df_security_monthly['FYEARQ'] >= min_year) & (df_security_monthly['FYEARQ'] <= max_year)]
+
+df_fundamentals_quarterly[df_fundamentals_quarterly['FYEARQ'] == 2024]
 # Summarize data
 df_1 = df_fundamentals_quarterly.drop(columns=['GVKEY', 'DATADATE', 'CONM', 'TIC', 'CIK', 'GSECTOR', 'LOC'])
 df_summary_1 = fn.tab_summary(df_1)
@@ -40,29 +69,7 @@ df_summary_1 = fn.tab_summary(df_1)
 df_2 = df_security_monthly.drop(columns=['GVKEY', 'DATADATE'])
 df_summary_2 = fn.tab_summary(df_2)
 
-# Create year and quarter cols
-df_security_monthly['FYEARQ'] = df_security_monthly['DATADATE'].dt.year.astype(float)
-df_security_monthly['FQTR'] = df_security_monthly['DATADATE'].dt.quarter.astype(float)
-
-ls_selected_cols_2 = ['GVKEY', 'DATADATE', 'FYEARQ', 'FQTR', 'CSHOQ', 'CSHTRM', 'PRCCM', 'TRT1M']
-df_security_monthly = df_security_monthly[ls_selected_cols_2]
-
-# Filter dates (min_year-)
-min_year = 1990
-df_fundamentals_quarterly = df_fundamentals_quarterly[df_fundamentals_quarterly['FYEARQ'] >= min_year]
-df_security_monthly = df_security_monthly[df_security_monthly['FYEARQ'] >= min_year]
-
-#
-#
-df_fundamentals_quarterly.isna().sum() / len(df_fundamentals_quarterly)
-df_security_monthly.isna().sum() / len(df_security_monthly)
-# Create year and quarter column
-# Filter dates (
-print(df_fundamentals_quarterly[['DATADATE', 'NIY']].isna().sum())
-print(df_fundamentals_quarterly[['DATADATE', 'NIQ']].isna().sum())
-print(df_fundamentals_quarterly['DATADATE'])
-print(df_fundamentals_quarterly.columns)
-print(df_fundamentals_quarterly.head(5))
-print(df_security_monthly.tail(500))
-print(df_fundamentals_quarterly['GSECTOR'].head(100))
-print(df_fundamentals_quarterly['GPOA'])
+# Filter liquidity
+# Filter stock exchanges
+# Merge
+# Missing values management
--- scripts/functions.py
+++ scripts/functions.py
@@ -17,11 +17,9 @@ paths.update({'data': Path.joinpath(paths.get('main'), 'data'),
 def tab_summary(df_data):
     df_summary = pd.DataFrame({'Count': df_data.count(),  # Count of non-missing values
                                'Missing Pct': (df_data.isna().sum() / len(df_data)),  # Missing values as percentage
-                               'Mean': df_data.mean(),
                                'Min': df_data.min(),
-                               '25%': df_data.quantile([0.25]),
-                               '50%': df_data.quantile([0.50]),
-                               '75%': df_data.quantile([0.75]),
+                               'Mean': df_data.mean(),
+                               'Median': df_data.median(),
                                'Max': df_data.max()})
     return df_summary
 
