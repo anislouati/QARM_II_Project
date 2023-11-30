6f82c315c55bb76de302bb02c76cf2fc3029dcca
alouati77 <louati.anis@outlook.com>
2023-11-30 05:03:51 +0100

    Data management

--- main.py
+++ main.py
@@ -18,63 +18,70 @@ pd.set_option('display.max_columns', 10)
 # Import raw data
 df_fundamentals_quarterly = pd.read_sas(Path.joinpath(paths.get('data'), 'crsp_compustat_merged_fundamentals_quarterly.sas7bdat'))
 df_security_monthly = pd.read_sas(Path.joinpath(paths.get('data'), 'crsp_compustat_merged_security_monthly.sas7bdat'))
-df_stock = pd.read_sas(Path.joinpath(paths.get('data'), 'crsp_security_files_monthly_stock.sas7bdat'))
+df_stock_monthly = pd.read_sas(Path.joinpath(paths.get('data'), 'crsp_security_files_monthly_stock.sas7bdat'))
 
 # Filter selected cols
-ls_selected_cols_1 = ['GVKEY', 'DATADATE', 'CONM', 'TIC', 'CIK', 'EXCHG', 'GSECTOR', 'LOC', 'ACTQ', 'ATQ',
+ls_selected_cols_1 = ['GVKEY', 'LPERMCO', 'DATADATE', 'CONM', 'TIC', 'EXCHG', 'GSECTOR', 'LOC', 'ACTQ', 'ATQ',
                       'CEQQ', 'CHEQ', 'COGSQ', 'CSTKQ', 'DLCQ', 'DLTTQ', 'DPQ', 'EPSFXQ', 'LCTQ', 'LTQ',
-                      'MIBTQ', 'NIMQ', 'NIQ', 'PIQ', 'PSTKQ', 'REQ', 'REVTQ', 'TEQQ', 'TXPQ', 'WCAPQ',
-                      'CAPXY', 'CSHTRQ', 'MKVALTQ', 'PRCCQ']
+                      'MIBTQ', 'NIQ', 'PIQ', 'PSTKQ', 'REQ', 'REVTQ', 'TXPQ', 'WCAPQ', 'CAPXY']
 df_fundamentals_quarterly = df_fundamentals_quarterly[ls_selected_cols_1]
 
-ls_selected_cols_2 = ['GVKEY', 'DATADATE', 'CSHOQ', 'CSHTRM', 'PRCCM', 'TRT1M']
+ls_selected_cols_2 = ['GVKEY', 'LPERMCO', 'DATADATE', 'TRT1M']
 df_security_monthly = df_security_monthly[ls_selected_cols_2]
 
-ls_selected_cols_3 = ['DATE', 'CUSIP', 'BID', 'ASK', 'VOL', 'SPREAD']
-df_stock = df_stock[ls_selected_cols_3]
+ls_selected_cols_3 = ['PERMCO', 'DATE', 'ASK', 'BID', 'VOL', 'SHROUT']
+df_stock_monthly = df_stock_monthly[ls_selected_cols_3]
 
 # Sort raw data
-df_fundamentals_quarterly = df_fundamentals_quarterly.sort_values(by=['GVKEY', 'DATADATE'], ascending=[True, True]).reset_index(drop=True)
-df_security_monthly = df_security_monthly.sort_values(by=['GVKEY', 'DATADATE'], ascending=[True, True]).reset_index(drop=True)
+df_fundamentals_quarterly = df_fundamentals_quarterly.sort_values(by=['LPERMCO', 'DATADATE'], ascending=[True, True]).reset_index(drop=True)
+df_security_monthly = df_security_monthly.sort_values(by=['LPERMCO', 'DATADATE'], ascending=[True, True]).reset_index(drop=True)
+df_stock_monthly = df_stock_monthly.sort_values(by=['PERMCO', 'DATE'], ascending=[True, True]).reset_index(drop=True)
 
-# Create fiscal year and quarter cols
-df_fundamentals_quarterly['FYEAR'] = df_fundamentals_quarterly['DATADATE'].dt.year.astype(float)
-df_fundamentals_quarterly['FQTR'] = df_fundamentals_quarterly['DATADATE'].dt.quarter.astype(float)
-ls_selected_cols_1 = ls_selected_cols_1[:2] + ['FYEAR', 'FQTR'] + ls_selected_cols_1[2:]
-df_fundamentals_quarterly = df_fundamentals_quarterly.reindex(columns=ls_selected_cols_1)
+# Create year, quarter and month cols
+df_fundamentals_quarterly['YEAR'] = df_fundamentals_quarterly['DATADATE'].dt.year.astype(float)
+df_fundamentals_quarterly['QTR'] = df_fundamentals_quarterly['DATADATE'].dt.quarter.astype(float)
+df_fundamentals_quarterly = df_fundamentals_quarterly[ls_selected_cols_1[:3] + ['YEAR', 'QTR'] + ls_selected_cols_1[3:]]
 
-df_security_monthly['FYEAR'] = df_security_monthly['DATADATE'].dt.year.astype(float)
-df_security_monthly['FQTR'] = df_security_monthly['DATADATE'].dt.quarter.astype(float)
-ls_selected_cols_2 = ls_selected_cols_2[:2] + ['FYEAR', 'FQTR'] + ls_selected_cols_2[2:]
-df_security_monthly = df_security_monthly.reindex(columns=ls_selected_cols_2)
+df_security_monthly['YEAR'] = df_security_monthly['DATADATE'].dt.year.astype(float)
+df_security_monthly['QTR'] = df_security_monthly['DATADATE'].dt.quarter.astype(float)
+df_security_monthly['MTH'] = df_security_monthly['DATADATE'].dt.month.astype(float)
+df_security_monthly = df_security_monthly[ls_selected_cols_2[:3] + ['YEAR', 'QTR', 'MTH'] + ls_selected_cols_2[3:]]
+
+df_stock_monthly['YEAR'] = df_stock_monthly['DATE'].dt.year.astype(float)
+df_stock_monthly['QTR'] = df_stock_monthly['DATE'].dt.quarter.astype(float)
+df_stock_monthly['MTH'] = df_stock_monthly['DATE'].dt.month.astype(float)
+df_stock_monthly = df_stock_monthly[ls_selected_cols_3[:2] + ['YEAR', 'QTR', 'MTH'] + ls_selected_cols_3[2:]]
 
 # Filter dates (min_year-max_year)
 min_year = 1990
 max_year = 2023
-df_fundamentals_quarterly = df_fundamentals_quarterly[(df_fundamentals_quarterly['FYEAR'] >= min_year) & (df_fundamentals_quarterly['FYEAR'] <= max_year)]
-df_security_monthly = df_security_monthly[(df_security_monthly['FYEAR'] >= min_year) & (df_security_monthly['FYEAR'] <= max_year)]
+df_fundamentals_quarterly = df_fundamentals_quarterly[(df_fundamentals_quarterly['YEAR'] >= min_year) & (df_fundamentals_quarterly['YEAR'] <= max_year)]
+df_security_monthly = df_security_monthly[(df_security_monthly['YEAR'] >= min_year) & (df_security_monthly['YEAR'] <= max_year)]
+df_stock_monthly = df_stock_monthly[(df_stock_monthly['YEAR'] >= min_year) & (df_stock_monthly['YEAR'] <= max_year)]
 
 # Filter stock exchanges (11: NYSE, 12: AMEX, 14: NASDAQ-NMS)
 df_fundamentals_quarterly = df_fundamentals_quarterly[df_fundamentals_quarterly['EXCHG'].isin([11., 12., 14.])]
-ls_gvkeys = df_fundamentals_quarterly['GVKEY'].unique()
-df_security_monthly = df_security_monthly[df_security_monthly['GVKEY'].isin(ls_gvkeys)]
+ls_permcos = df_fundamentals_quarterly['LPERMCO'].unique().tolist()
+df_security_monthly = df_security_monthly[df_security_monthly['LPERMCO'].isin(ls_permcos)]
+df_stock_monthly = df_stock_monthly[df_stock_monthly['PERMCO'].isin(ls_permcos)]
+
+# Filter out illiquid stocks
+
+# Summarize data
+df_1 = df_fundamentals_quarterly.drop(columns=['GVKEY', 'LPERMCO', 'DATADATE', 'CONM', 'TIC', 'GSECTOR', 'LOC'])
+df_summary_1 = fn.tab_summary(df_1)
+
+df_2 = df_security_monthly.drop(columns=['GVKEY', 'LPERMCO', 'DATADATE'])
+df_summary_2 = fn.tab_summary(df_2)
+
+df_3 = df_stock_monthly.drop(columns=['PERMCO', 'DATE'])
+df_summary_3 = fn.tab_summary(df_3)
+
 
 
 
 df_fundamentals_quarterly['DolVol'] = df_fundamentals_quarterly['']
-
 df_security_monthly['DolVol'] = df_security_monthly['CSHTRM'] * df_security_monthly['PRCCM']
-
-
-
-
-# Summarize data
-df_1 = df_fundamentals_quarterly.drop(columns=['GVKEY', 'DATADATE', 'CONM', 'TIC', 'CIK', 'GSECTOR', 'LOC'])
-df_summary_1 = fn.tab_summary(df_1)
-
-df_2 = df_security_monthly.drop(columns=['GVKEY', 'DATADATE'])
-df_summary_2 = fn.tab_summary(df_2)
-
 
 AUM = 10AUM
 2%*AUM <= 10%*(DolVol / 21)
