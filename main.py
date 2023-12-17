77816f0c4d48c1cff2b079f880c2bf6f6c012cbc
alouati77 <louati.anis@outlook.com>
2023-12-17 16:23:45 +0100

    Sync changes

--- main.py
+++ main.py
@@ -232,27 +232,48 @@ ls_values = [812, 1620, 3240, 818, 1626, 3246, 830, 1638, 4070,
              3240, 5580, 1626, 5676, 4070, 5688,
              3240, 3780, 1626, 3786, 4070, 3888]
 dic_selected_ports = dict(zip(ls_keys, ls_values))
-dic_sigs = {'VAL': 'ZS_VAL', 'QLT': 'ZS_QLT', 'VQ': 'ZS_VAL_QLT', 'VQAM': 'ZS_VAL_QLT_AMOM'}
 
-# Export ports perfs (uncomment)
-'''
-for i in tqdm(range(len(ls_keys)), desc='Export ports perfs'):
-    s_tmp = df_ports_chars.iloc[dic_selected_ports[ls_keys[i]]]
-    port = Portfolio(dic_data=dic_data, sig_long=dic_sigs[s_tmp['L_SIG']], n_asts_long=s_tmp['L_N_ASTS'], w_meth_long=s_tmp['L_W_METH'], pct_long=s_tmp['L_PCT'],
-                     sig_short=dic_sigs[s_tmp['S_SIG']], n_asts_short=s_tmp['S_N_ASTS'], w_meth_short=s_tmp['S_W_METH'], pct_short=s_tmp['S_PCT'],
-                     ind_const=s_tmp['IND_CONST'], reb_freq=s_tmp['REB_FREQ'], tc_bps=20)
-    df_port_perf = port.tab_port_perf()
-    df_port_perf.to_excel(Path.joinpath(paths.get('output'), 'excels', 'df_port_perf_{}.xlsx'.format(ls_keys[i])), index=True)
-'''
 
-def tab_ports_perfs(ls_ports, file_name):
+# Export ports navs
+
+
+
+
+def tab_ports_feats(dic_selected_ports, ls_feats, file_name):
     with open(Path.joinpath(paths.get('data'), 'dic_data.pkl'), 'rb') as file:
         dic_data = pickle.load(file)
+    with open(Path.joinpath(paths.get('tables'), 'df_ports_chars.pkl'), 'rb') as file:
+        df_ports_chars = pickle.load(file)
+    ls_keys = list(dic_selected_ports.keys())
+    dic_sigs = {'VAL': 'ZS_VAL', 'QLT': 'ZS_QLT', 'VQ': 'ZS_VAL_QLT', 'VQAM': 'ZS_VAL_QLT_AMOM'}
+
+    df_ports_feats = pd.DataFrame()
+    for i in tqdm(range(len(ls_keys)), desc='Export ports feats'):
+        s_tmp = df_ports_chars.iloc[dic_selected_ports[ls_keys[i]]]
+        port = Portfolio(dic_data=dic_data, sig_long=dic_sigs[s_tmp['L_SIG']], n_asts_long=s_tmp['L_N_ASTS'], w_meth_long=s_tmp['L_W_METH'], pct_long=s_tmp['L_PCT'],
+                         sig_short=dic_sigs[s_tmp['S_SIG']], n_asts_short=s_tmp['S_N_ASTS'], w_meth_short=s_tmp['S_W_METH'], pct_short=s_tmp['S_PCT'],
+                         ind_const=s_tmp['IND_CONST'], reb_freq=s_tmp['REB_FREQ'])
+        df_port_perf = port.tab_port_perf()
+        if i == 0:
+            df_ports_feats = pd.concat([df_ports_feats, df_port_perf[['DATE'] + ls_feats]], axis=1, ignore_index=True)
+        else:
+            df_ports_feats = pd.concat([df_ports_feats, df_port_perf[ls_feats]], axis=1, ignore_index=True)
+    df_ports_feats.to_excel(Path.joinpath(paths.get('tables'), '{}.xlsx'.format(file_name)), index=True)
+
+
+
+tab_ports_feats({key: dic_selected_ports[key] for key in ['VAL_1', 'VAL_2', 'VAL_3'] if key in dic_selected_ports},
+                ls_feats=['PORT_NAV'], file_name='df_ports_navs_1')
 
 
 
 
 
+
+
+
+dic_tmp = dic_selected_ports[[]]
+restricted_dict =
 # Export ports stats
 def tab_port_stats(list_port, file_name):
     dic_ports_stats = {}
